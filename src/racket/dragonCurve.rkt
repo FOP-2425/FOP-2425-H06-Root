51da6bda6e94646c9dc1e6d34c7c6ea39fbb756c
Manuel Peters <manuel.peters@stud.tu-darmstadt.de>
2024-06-17 02:14:21 +0200

    add dragonCurve.rkt

---
+++ src/racket/dragonCurve.rkt
@@ -0,0 +1,44 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname dragonCurve) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Alle 4 Fkt. implementieren lassen!
+
+;; natural -> natural
+;; Rekursiv implementieren lassen in Java!!!
+(define (power a b)
+  (cond
+    [(= b 0) 1]
+    [(= b 1) a]
+    [else (* a (power a (- b 1)))]))
+
+
+;;; keyword append war nicht in der VL dran, deswegen definieren wir es selbst (mit anderem Namen)
+;; Das hier iterativ in Java implementieren lassen!!!! Zwingend iterativ!!!
+(define (append-lists lst1 lst2)
+  (if (empty? lst1) lst2
+      (cons (first lst1) (append-lists (rest lst1) lst2))))
+
+
+;; define contract s.t. idx has to be a valid index
+;; Auch iterativ implementieren lassen!!!
+(define (replace-at-idx lst idx elem)
+  (cond
+    [(= idx 0) (cons elem (rest lst))]
+    [else (cons (first lst) (replace-at-idx (rest lst) (- idx 1) elem))]))
+
+
+;; Obviously rekursiv implementieren lassen!!
+(define (dragon order)
+  (if (= order 1)
+      (list 'R)
+      (append-lists (append-lists (dragon (- order 1)) (list 'R)) (replace-at-idx (dragon (- order 1)) (- (power 2 (- order 2)) 1) 'L))))
+
+
+
+;;;;;;;;;;;;;;;;;;;
+(dragon 3)
+(length (dragon 4))
+
+(append-lists '(1 2 3) '(4 5 6))
+
+(power 2 6)
--- src/racket/kochFractal.rkt
+++ src/racket/kochFractal.rkt
@@ -50,11 +50,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-struct segment
   (x1 y1 x2 y2))
-
+ 
 (define (draw-segment g seg)
   (send g draw-line (segment-x1 seg) (segment-y1 seg) (segment-x2 seg) (segment-y2 seg))
-)
-
+  )
+ 
 (define (draw-koch-snowflake g depth seg)
   (cond [(= depth 0) (draw-segment g seg)]
         [else (local ((define dx (/ (- (segment-x2 seg) (segment-x1 seg)) 3.0))
@@ -69,7 +69,7 @@
                        (draw-koch-snowflake g (- depth 1) (make-segment xA yA xC yC))
                        (draw-koch-snowflake g (- depth 1) (make-segment xC yC xB yB))
                        (draw-koch-snowflake g (- depth 1) (make-segment xB yB (segment-x2 seg) (segment-y2 seg)))))]))
-
+ 
 (define (draw-fractal g width height)
   (draw-koch-snowflake g DEPTH (make-segment (/ width 4) (/ (+ height INITIAL-LENGTH) 2) (/ width 2) (/ (+ height INITIAL-LENGTH) 2))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -80,47 +80,51 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; PROBLEM: Man muss die Listen mit append zusammenfügen, nicht mit cons !!!
-;; (cons '(1 2) '(3 4)) becomes '((1 2) 4 5)
+;; (cons '(1 2) '(3 4)) becomes '((1 2) 3 4)
+
+
+
+
 ;; append wurde nie eingeführt in VL
-;; (define-struct segment
-;;   (x1 y1 x2 y2))
-;; 
-;; (define (draw-segments g segs)
-;;   (for ([i segs]) (draw-line g i)))
-;; 
-;; (define (draw-line g seg)
-;;   (send g draw-line (segment-x1 seg) (segment-y1 seg) (segment-x2 seg) (segment-y2 seg))
-;; )
-;; 
-;; (define (generate-koch-snowflake g depth seg)
-;;   (cond [(= depth 0) (list seg)]
-;;         [else (local ((define dx (/ (- (segment-x2 seg) (segment-x1 seg)) 3.0))
-;;                       (define dy (/ (- (segment-y2 seg) (segment-y1 seg)) 3.0))
-;;                       (define xA (+ (segment-x1 seg) dx))
-;;                       (define yA (+ (segment-y1 seg) dy))
-;;                       (define xB (- (segment-x2 seg) dx))
-;;                       (define yB (- (segment-y2 seg) dy))
-;;                       (define xC (- (+ xA (* dx (cos 60°))) (* dy (sin 60°))))
-;;                       (define yC (+ (+ yA (* dx (sin 60°))) (* dy (cos 60°)))))
-;;                 (append (generate-koch-snowflake g (- depth 1) (make-segment (segment-x1 seg) (segment-y1 seg) xA yA))
-;;                         (generate-koch-snowflake g (- depth 1) (make-segment xA yA xC yC))
-;;                         (generate-koch-snowflake g (- depth 1) (make-segment xC yC xB yB))
-;;                         (generate-koch-snowflake g (- depth 1) (make-segment xB yB (segment-x2 seg) (segment-y2 seg)))) 
-;;                        )]))
-;; 
-;; (define (draw-fractal g width height)
-;;   (draw-segments g (generate-koch-snowflake g DEPTH (make-segment (/ width 4) (/ (+ height INITIAL-LENGTH) 2) (/ width 2) (/ (+ height INITIAL-LENGTH) 2)))))
+;(define-struct segment
+;  (x1 y1 x2 y2))
+;
+;(define (draw-segments g segs)
+;  (for ([i segs]) (draw-line g i)))
+;
+;(define (draw-line g seg)
+;  (send g draw-line (segment-x1 seg) (segment-y1 seg) (segment-x2 seg) (segment-y2 seg))
+;)
+;
+;(define (generate-koch-snowflake g depth seg)
+;  (cond [(= depth 0) (list seg)]
+;        [else (local ((define dx (/ (- (segment-x2 seg) (segment-x1 seg)) 3.0))
+;                      (define dy (/ (- (segment-y2 seg) (segment-y1 seg)) 3.0))
+;                      (define xA (+ (segment-x1 seg) dx))
+;                      (define yA (+ (segment-y1 seg) dy))
+;                      (define xB (- (segment-x2 seg) dx))
+;                      (define yB (- (segment-y2 seg) dy))
+;                      (define xC (- (+ xA (* dx (cos 60°))) (* dy (sin 60°))))
+;                      (define yC (+ (+ yA (* dx (sin 60°))) (* dy (cos 60°)))))
+;                (append (generate-koch-snowflake g (- depth 1) (make-segment (segment-x1 seg) (segment-y1 seg) xA yA))
+;                        (generate-koch-snowflake g (- depth 1) (make-segment xA yA xC yC))
+;                        (generate-koch-snowflake g (- depth 1) (make-segment xC yC xB yB))
+;                        (generate-koch-snowflake g (- depth 1) (make-segment xB yB (segment-x2 seg) (segment-y2 seg)))) 
+;                       )]))
+;
+;(define (draw-fractal g width height)
+;  (draw-segments g (generate-koch-snowflake g DEPTH (make-segment (/ width 4) (/ (+ height INITIAL-LENGTH) 2) (/ width 2) (/ (+ height INITIAL-LENGTH) 2)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (define frame (new frame%
-                 [label "Koch Snowflake"]
-                 [width 800]
-                 [height 600]))
+                   [label "Koch Snowflake"]
+                   [width 800]
+                   [height 600]))
 
 (define canvas (new canvas%
-                  [parent frame]
-                  [paint-callback (lambda (canvas g)
-                                    (draw-fractal g (send canvas get-width) (send canvas get-height)))]))
+                    [parent frame]
+                    [paint-callback (lambda (canvas g)
+                                      (draw-fractal g (send canvas get-width) (send canvas get-height)))]))
 
 (send frame show #t)
