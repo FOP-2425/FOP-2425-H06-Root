df8b8cb7f52c6f62c0e18de4860c04e32c92e97c
niklhut <49069026+niklhut@users.noreply.github.com>
2024-06-19 15:50:03 +0200

    Improve racket method contract

--- src/racket/linearSearch.rkt
+++ src/racket/linearSearch.rkt
@@ -1,11 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname linearSearch) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; Takes a list and a target value and returns the index of the target value in the list.
 ;; If the target value is not in the list, returns -1.
-;; Type: (list Any) Any -> Number
+;; Type: (list integer) integer -> integer
+;; Returns: Index of first occurence of target or -1 if target is not in list.
 (define (linear-search lst target)
   (local ((define (linear-search-helper lst target index)
     (cond
       [(empty? lst) -1]
-      [(equal? (first lst) target) index]
+      [(= (first lst) target) index]
       [else (linear-search-helper (rest lst) target (+ index 1))])))
   (linear-search-helper lst target 0)))
 
